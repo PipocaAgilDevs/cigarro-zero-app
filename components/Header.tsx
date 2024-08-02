fa02547a70fe1e34131705649973949f06253190
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-08-02 18:03:49 -0300

    Header clean code and colors

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,15 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Modal } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const HeaderHome: React.FC = () => {
-  const [searchModalVisible, setSearchModalVisible] = useState(false);
-  const [notificationModalVisible, setNotificationModalVisible] =
-    useState(false);
-
-  const handleBellPress = () => {
-    setNotificationModalVisible(true);
-  };
-
-  const handleSearchPress = () => {
-    setSearchModalVisible(true);
-  };
-
   return (
     <>
       <View style={styles.containerOuter}>
         <Text style={styles.title}>Cigarro Zero</Text>
-        <TouchableOpacity onPress={handleSearchPress}>
-          <Icon name="search" size={18} color="#F7A833" />
+        <Text>Meu Progresso</Text>
+        <TouchableOpacity>
+          <Icon name="ellipsis-vertical-sharp" size={16} color="#000000" />
         </TouchableOpacity>
       </View>
     </>
@@ -35,6 +24,8 @@ const styles = StyleSheet.create({
     marginBottom: 26,
     paddingLeft: 22,
     paddingRight: 16,
+    borderBottomWidth: 1,
+    height: 56,
   },
   title: {
     fontSize: 14,
