244e940f1c1d797c7f4a8480e11583d1fc676fb7
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-08-02 17:50:36 -0300

    header clean code

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Modal,
-  TextInput,
-} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Modal } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const HeaderHome: React.FC = () => {
@@ -25,49 +18,11 @@ const HeaderHome: React.FC = () => {
   return (
     <>
       <View style={styles.containerOuter}>
-        <TouchableOpacity onPress={handleBellPress}>
-          <Icon name="bell" size={12} color="#F7A833" />
-        </TouchableOpacity>
         <Text style={styles.title}>Cigarro Zero</Text>
         <TouchableOpacity onPress={handleSearchPress}>
           <Icon name="search" size={18} color="#F7A833" />
         </TouchableOpacity>
       </View>
-
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={searchModalVisible}
-        onRequestClose={() => setSearchModalVisible(false)}
-      >
-        <TouchableOpacity
-          style={styles.modalBackground}
-          activeOpacity={1}
-          onPress={() => setSearchModalVisible(false)}
-        >
-          <View style={styles.searchModalContainer}>
-            <View style={styles.searchModalContent}></View>
-          </View>
-        </TouchableOpacity>
-      </Modal>
-
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={notificationModalVisible}
-        onRequestClose={() => setNotificationModalVisible(false)}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <Text style={styles.modalText}>Não há notificações</Text>
-            <TouchableOpacity
-              onPress={() => setNotificationModalVisible(false)}
-            >
-              <Text style={styles.closeButton}>Fechar</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </Modal>
     </>
   );
 };
@@ -86,48 +41,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#323232",
     fontFamily: "Nunito_700Bold",
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  modalContent: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    alignItems: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
-  searchModalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  searchModalContent: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-  },
-  searchInput: {
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: "#000",
-    padding: 10,
-    borderRadius: 10,
-    width: 300,
-    fontFamily: "Nunito_400Regular",
-  },
-  modalBackground: {
-    flex: 1,
-  },
-  closeButton: {
-    color: "#F7A833",
-    fontFamily: "Nunito_400Regular",
   },
 });
 
