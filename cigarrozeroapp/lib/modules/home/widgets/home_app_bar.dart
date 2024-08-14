037b626d13ec2b45ab6f0c8069e5b8821b85ee93
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-08-14 18:40:28 -0300

    header stilization and font import

--- cigarrozeroapp/lib/modules/home/widgets/home_app_bar.dart
+++ cigarrozeroapp/lib/modules/home/widgets/home_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeAppBar extends StatelessWidget {
   const HomeAppBar({super.key});
@@ -6,15 +7,51 @@ class HomeAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.red,
-      centerTitle: true,
-      title: const Column(
-        children: [
-          Text('Cigarro Zero'),
-          Text(
-            'Meu progresso',
-          ),
-        ],
+      backgroundColor: Colors.white,
+      title: SizedBox(
+        width: double.infinity,
+        height: kToolbarHeight,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "Cigarro Zero",
+                      style: TextStyle(
+                        color: Color(0xFF101828),
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Oswald',
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    Text(
+                      "Meu Progresso",
+                      style: TextStyle(
+                        color: Color(0xFF475467).withOpacity(0.6),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              bottom: 0,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert),
+                color: const Color(0xFF000000),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
